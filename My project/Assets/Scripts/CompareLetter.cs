702cbae992549edce5a44d74c395cdac0ecf48ea
Alexia van den Berg <alexia.vdberg@gmail.com>
2023-10-26 16:27:28 +0200

    mechanics done

--- My project/Assets/Scripts/CompareLetter.cs
+++ My project/Assets/Scripts/CompareLetter.cs
@@ -5,18 +5,28 @@ using TMPro;
 
 public class CompareLetter : MonoBehaviour
 {
-    private string haiku = "bonjour";
+    private string haiku = $"Téléphone en main";
     private int letterIndex = 0;
     private char typedLetter;
-    public float range;
+    private char currentLetter;
+    private float range;
     public Pulse pulse;
+    public TextMeshProUGUI haiku_text;
+    private string debut;  // Texte correctement saisi
+    private string fin;  // Texte restant à saisir
+
+    private void Start()
+    {
+        debut = "";
+        fin = haiku;
+        UpdateTextMeshPro();  // Mise à jour du TextMeshProUGUI
+    }
 
     private void OnGUI()
     {
         Event e = Event.current;
         if (e.isKey)
         {
-            // Debug.Log("Detected key code: " + e.keyCode);
             typedLetter = e.character; // Obtenir la lettre de la touche pressée
         }
         else
@@ -28,37 +38,52 @@ public class CompareLetter : MonoBehaviour
     private void Update()
     {
         range = pulse.range;
+        currentLetter = fin[0];  // La lettre actuelle est celle à l'indice 0 du texte restant
 
-        char currentLetter = haiku[letterIndex];
-
-        // Faites défiler les lettres de la phrase en fonction du temps
-        if ((range >= 3f && range < 3.5f || range > 4.5f && range <= 5f) && typedLetter != (char)0)
+        if ((range >= 3f && range < 3.2f || range > 4.8f && range <= 5f) && typedLetter != (char)0)
         {
-            // char inputLetter = Input.inputString[0];
-            Debug.Log($"Entré : {typedLetter}  Attendu :{currentLetter}");
-
+            // Debug.Log($"Entré : {typedLetter}  Attendu : {currentLetter}");
 
             if (typedLetter == currentLetter)
             {
-                Debug.Log($"Lettre correcte : {currentLetter}");
+                CorrectLetter();
             }
             else
             {
-                Debug.Log($"Lettre incorrecte : {typedLetter}");
+                // Debug.Log($"Lettre incorrecte : {typedLetter}");
+                IncorrectLetter();
             }
-            letterIndex++;
         }
-        else if ((range >= 3.5f && range <= 4.5f) && typedLetter != (char)0)
+        else if ((range >= 3.2f && range <= 4.8f) && typedLetter != (char)0)
         {
-            Debug.Log($"Mauvais timing : {currentLetter}");
-            letterIndex++;
+            // Debug.Log($"Mauvais timing : {currentLetter}");
+            IncorrectLetter();
         }
 
-        if (letterIndex == haiku.Length)
+        if (fin.Length == 0)
         {
             Debug.Log("Finito!");
         }
 
         typedLetter = (char)0;
+    }
+
+    private void UpdateTextMeshPro()
+    {
+        haiku_text.text = $"{debut}{fin}";
+    }
+
+    private void CorrectLetter()
+    {
+        debut += $"<color=green>{currentLetter}</color>";  // Ajoute la lettre correcte au texte correctement saisi
+        fin = fin.Substring(1);  // Enlève la lettre correcte du texte restant
+        UpdateTextMeshPro();  // Mise à jour de TextMeshProUGUI
+    }
+
+    private void IncorrectLetter()
+    {
+        debut += $"<color=red>{currentLetter}</color>";  // Ajoute la lettre correcte au texte correctement saisi
+        fin = fin.Substring(1);  // Enlève la lettre correcte du texte restant
+        UpdateTextMeshPro();  // Mise à jour de TextMeshProUGUI
     }
 }
